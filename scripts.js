df3fb1bdd3ab665f0e038c71d49646b136e8c56c
Valzionn <144949469+Valzionn@users.noreply.github.com>
2024-03-18 16:31:06 +0000

    a

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+document.getElementById("homeButton").onclick = function () {
+  location.href = "frontPage.html";
+};
+
+
 document.getElementById("menuButton").onclick = function () {
     location.href = "menu.html";
   };
+  
